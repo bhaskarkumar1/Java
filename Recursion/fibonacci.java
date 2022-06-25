801c2902f55f34b63d97e58f0efb4fd20f926057
Bhaskar <bhaskarkumar.india@gmail.com>
2022-06-25 11:30:11 +0530

    fibonacci

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -1,7 +1,19 @@
 class fibonacci{
  public static void main(String[] args) {
-   System.out.println("Hello World !!!!!!");
+ fibonacci recursion=new fibonacci();
+ var rec= recursion.fib(-4);
+ System.out.println(rec);
  }
+public int fib(int n){
+  if(n==0 ||n==1){
+    return n;
+  }
+  if (n<0){
+    return -1;
+  }
+return fib(n-1)+fib(n-2);
 
+
+}
 
 }
