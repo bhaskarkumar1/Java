1f694b8b6ff89973293a62729bcece9d9ecc8563
Bhaskar <bhaskarkumar.india@gmail.com>
2022-06-26 14:48:47 +0530

    Create arrays

---
+++ arrays/Createarrays.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Createarrays{
+ public static void main(String[] args) {
+  int[] arr;//declare O1
+  arr= new int[3];// create arrays O1
+  arr[0]=0;//O1
+  arr[1]=1;//O1 //all together O3
+  arr[2]=2;//O1
+System.out.println(Arrays.toString(arr));
+//All together
+String array[]={"a","b","c","d"};
+System.out.println(Arrays.toString(array));
+
+
+}
+
+
+
+}
