356e41e04e231e35021d9da2180ea99273c63c68
Bhaskar <bhaskarkumar.india@gmail.com>
2022-06-27 13:10:50 +0530

    Create 2D array

---
+++ arrays/Main2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Main2{
+  public static void main (String[] args){
+    // declare
+    int[][] int2darray; //O1
+    //instantiate
+    int2darray=new int[2][2]; //O1
+    // intialization
+    int2darray[0][0]=1;
+    int2darray[0][1]=2;
+    int2darray[0][1]=3; //all together O(MN)
+    int2darray[1][1]=4;
+    System.out.println(Arrays.deepToString(int2darray));
+
+    // all Together
+    String s2dArray[][]={{"A","B"},{"C","D"}}; //O1
+    System.out.println(Arrays.deepToString(s2dArray));
+ // but space complexity is Omn
+  }
+
+
+}
