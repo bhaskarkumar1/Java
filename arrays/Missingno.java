698df38ff22a7ae2630b0aee9866a6062f88115f
Bhaskar <bhaskarkumar.india@gmail.com>
2022-06-29 20:49:30 +0530

    Misssing number

---
+++ arrays/Missingno.java
@@ -0,0 +1,15 @@
+class Missingno{
+  public static void main(String[]args)  {
+    int intarr[]={1,2,3,4,5,6,8,9,10};
+int sum1=0;
+int sum2=0;
+sum2=(10*11)/2;
+    for(int i=0;i<intarr.length; i++){
+      sum1+=intarr[i];
+
+    }
+    System.out.print("Missing Number is : " + (sum2-sum1));
+  }
+
+
+}
