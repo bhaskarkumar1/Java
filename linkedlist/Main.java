b37af051137faf4247416ae41a62c1624eddfc7d
Bhaskar <bhaskarkumar.india@gmail.com>
2022-07-28 22:26:18 +0530

    Singly Linked List completed...

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -3,6 +3,15 @@ class Main{
     Singlylinkedlist sLL= new Singlylinkedlist();
     sLL.createSinglyLinkedList(5);
     System.out.println(sLL.head.value);
-    }
+    sLL.insertionList(6,0);
+    sLL.insertionList(7,3);
+    System.out.println(sLL.head.value);
+    sLL.traversalMethod();
+    sLL.searchingMethod(7);
+    sLL.deleteNode(3);
+    sLL.traversalMethod(); 
+    sLL.deleteEntire();
+    sLL.traversalMethod();   
+}
 
 }
--- linkedlist/Singlylinkedlist.java
+++ linkedlist/Singlylinkedlist.java
@@ -15,4 +15,111 @@ public class Singlylinkedlist{
 
         return head;
     }
+
+    //Insertion method
+    public void insertionList(int nodeValue, int location){
+        Node node=new Node();
+        node.value=nodeValue;
+        if(head==null){
+            createSinglyLinkedList(nodeValue);
+            return ;
+        
+        }else if(location==0){
+            node.next=head;
+            head=node;
+        }else if(location>=size){
+            node.next=null;
+            tail.next=node;
+            tail=node;
+        }else{
+            Node tempNode=head;
+            int index=0;
+            while(index<location-1){
+                tempNode=tempNode.next;
+                index++;
+            }
+            Node nextNode=tempNode.next;
+            tempNode.next=node;
+            node.next=nextNode;
+        }
+        size++;
+    }
+    //traversal
+    public void traversalMethod(){
+        Node tempNode=head;
+        for(int i=0; i<size; i++){
+            System.out.print(tempNode.value);
+            tempNode=tempNode.next;
+            if(i!=size-1){
+                System.out.print("->");
+            }
+        }
+
+    }
+    //Searching Method
+     boolean searchingMethod(int nodeValue){
+        if(head!=null){
+
+            Node tempNode= head;
+            for(int j =0;j<size;j++){
+                if(tempNode.value==nodeValue){
+                    System.out.println("Node found at" +j+"\n");
+    
+                    return true;
+                }
+                tempNode=tempNode.next;
+            }
+           
+        }
+    
+
+        System.out.println("Node not found");
+        return false;
+
+    }
+// deleting a node from SinglyLinkedlist
+public void deleteNode(int location){
+    if (head==null){
+        System.out.println("SLL does not exits");
+        return;
+    }else if (location==0){
+        head=head.next;
+        size--;
+        if(size==0){
+            tail=null;
+        }
+    }
+            else if(location>=size)  {
+                Node tempNode=head;
+                for(int k=0;k<size-1;k++){
+                    tempNode=tempNode.next;
+                }
+                if(tempNode==head){
+                    head=tail=null;
+                    size--;
+                }
+                tempNode.next=null;
+                tail=tempNode;
+                size--;
+
+           }else{
+            Node tempNode=head;
+            for(int i=0; i<location-1;i++){
+                tempNode=tempNode.next;
+            }
+            tempNode.next=tempNode.next.next;
+            size--;
+           }
+    
+}
+// delete entire SLL
+public void deleteEntire(){
+    head=null;
+    tail=null;
+    System.out.println("sll Deleted Successfully");
+}
+
+
+
+
 }
